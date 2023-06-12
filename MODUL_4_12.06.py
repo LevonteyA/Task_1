9d82af59c2501536f4e185169a7eb8c9c4ad7fe0
Levontey <gorya4eva_o@mail.ru>
2023-06-12 18:41:49 +0300

    first_commit

---
+++ MODUL_4_12.06.py
@@ -0,0 +1,21 @@
+#анализ сложности алгоритмов Big O
+#счётчик уникальных символов
+#функция считает сколько раз в строке встречается символ
+#line - строка
+import time
+def count_sym(line):
+    #set - функция возвращающая множество
+    #множество - тип данных без повтроряющихся элементов
+    for sym in set(line):
+        #counter - счётчик
+        counter = 0
+        for sub_sym in line:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+
+#замеряем время начала
+start = time.time()
+count_sym("Ghoul horse" * 100)
+finish = time.time()
+print('Время выполнения программы: ',finish-start)
